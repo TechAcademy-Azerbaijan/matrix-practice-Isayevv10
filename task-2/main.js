fabecab917b1035c619034b944c5e68260e911e9
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-17 11:17:10 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -7,9 +7,17 @@ let matrix = [
 let r = 3;
 let c = 2;
 let arr2 = [];
+let arr3 = [];
 for (let i = 0; i < r; i++) {
     for (let j = 0; j < c; j++) {
+
         arr2.push(matrix[i][j])
+
     }   
+    arr3.push(arr2);
+    arr2 = [];
 }
-console.log(arr2.join(' '));
+
+for (let item of arr3) {
+    console.log(item.join(' '));
+}
