e28214a8f5cb038182ebb207ef9a3383ad917a54
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-06-28 01:15:19 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let arr = parseInt(result.input.split(',')[0].split(' '));
+    let matrix = [];
+    for (let i = 0; i < arr; i++) {
+       matrix.push(new Array(arr).fill('*'));
+    }
+    for (let item of matrix) {
+        console.log(item.join(' '));
+    }
   
 });
